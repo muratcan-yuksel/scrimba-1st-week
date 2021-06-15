17a3ccf39e50ddc82477c141e62d1b43f4f70b89
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-06-15 06:37:50 +0300

    styling left

--- app.js
+++ app.js
@@ -6,39 +6,25 @@ const people= document.querySelector("#people_value")
 const tip= document.querySelector("#tip_value")
 //get total result area
 const result= document.querySelector("#total")
+//get per person area
+const perPerson= document.querySelector("#per_person")
 //change event to the bill
 function calculate (){
     console.log(bill.value)
  
     if (tip.value !=0 || tip.value >0 ){
-       
-        let foo=  parseFloat(((bill.value / 100) * tip.value)).toFixed(1)
-        let total= Number(foo) + Number(bill.value)
+       //calculate the total result 
+        let totalResult=  parseFloat(((bill.value / 100) * tip.value)).toFixed(1)
+        let total= Number(totalResult) + Number(bill.value)
         result.textContent= "$" + total ;
-
-
-
-        //get rid of the infinity bug
+        //divide it by the amount of people specified
+        perResult= total / Number(people.value)
+        perPerson.textContent= "$" + perResult.toFixed(1)
+         //get rid of the infinity bug
         if(result.textContent== "$Infinity"){
             result.textContent="$"
+        }else if ( perPerson.textContent=="$Infinity"){
+            perPerson.textContent="$"
         }
     }
 } 
-// //change event to the amount of people
-// function pplFunction(){
-// console.log(people.value)
-// if (tip.value !=0 || tip.value >0 ){
-//     result.textContent= "$" + ((bill.value / tip.value).toFixed(1) / people.value);
-
-// }
-
-// }
-
-// //change event to the tip
-// function tipFunction(){
-    
-// if (tip.value !=0 || tip.value >0 ){
-//     result.textContent= "$" + ((bill.value / tip.value)/ people.value).toFixed(1) ;
-
-// }
-// }
